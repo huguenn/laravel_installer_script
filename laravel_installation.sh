e6a79beddea00cbc53e572db6da93b7409838cd5
Shaon <shamim.hasan@gagagugu.com>
2016-07-25 18:27:48 +0600

    PHP installation and php version checking added

--- laravel_installation.sh
+++ laravel_installation.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-# check php version and extensions
+#global values
+required_version="5.5.9"
 
+#check for php installation
+
+if ! type "php" > /dev/null; then
+  echo "You don't have php installed, please install PHP first and then try again"
+  exit 1
+fi
+
+#check php version
+php_version=$(php -v | grep -P -o -i "PHP (\d+\.\d+\.\d+)" | tr -d "\n\r\f" | sed 's/PHP //g' )
+
+if [[ $php_version < $required_version ]]
+then
+	echo "You need to upgrade your PHP version to work with Laravel. Required version: $required_version"
+	exit 1
+fi
+
+
+#check for php extensions
 if ! [ "$(php -m | grep -c 'mbstring')" -ge 1 ]; then
 	echo "Please enable 'mbstring' php extension to proceed"
 	exit 1
@@ -26,14 +45,13 @@ fi
 
 
 
-
 # if a project name given then work on that otherwise prompt to give a project name
 if [ "$1" == "" ]; then
 	echo "Please give a project name."
 else
 	cd /var/www/html
 	composer create-project --prefer-dist laravel/laravel $1
-	#apache user group permission
+	#apache user group permission 
 	chown -R www-data.www-data /var/www/$1 
 	chmod -R 755 /var/www/$1
 	cd $1 
@@ -41,7 +59,7 @@ else
 	chmod -R 777 bootstrap/cache	
 	cp .env.example .env
 	php artisan key:generate
-	echo "Everything is ready mate!"
+	echo "Everything is ready, mate! Create something awesome!"
 
 #create virtual host
 fi
